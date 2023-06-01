0cdb3f9dd5bde66788813510e42cd06c512f1d58
MuhammadReza26 <rezazafirah66@gmail.com>
2023-06-01 13:34:56 +0800

    feat: menambahkan class baru Color.java

---
+++ Gradle_Proyek/app/src/main/helpers/Color.java
@@ -0,0 +1,10 @@
+package gradle_proyek.helpers;
+
+public class Color {
+
+    public final static String Blue = "#00FDFF";
+    public final static String Green = "#00FF4D";
+    public final static String Orange = "#FFBB00";
+    public final static String Red = "#FF0000";
+    public final static String Ungu = "#B100FF";
+}
