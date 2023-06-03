18b572f16eb9ad2ea61d88676fdc6702c3247729
MuhammadReza26 <rezazafirah66@gmail.com>
2023-06-03 20:51:04 +0800

    feat: menambahkan style untuk scene terakhir

--- Gradle_Proyek/app/src/main/java/gradle_proyek/Scene/startScene.java
+++ Gradle_Proyek/app/src/main/java/gradle_proyek/Scene/startScene.java
@@ -2,7 +2,6 @@ package gradle_proyek.Scene;
 
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
